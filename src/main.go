7586c02d4567e9313a969df17ca3d78f0b44493a
Corentin Recanzone <corentindevpro@hotmail.com>
2024-10-28 22:29:09 +0100

    refactor: refactoring move logic main.go to processor.go

--- src/main.go
+++ src/main.go
@@ -1,41 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-
-	_ "github.com/gocolly/colly/v2"
-	"github.com/joho/godotenv"
-	_ "github.com/nats-io/nats.go"
-)
-
 // Point d'entrée de l'application
 func main() {
-	// Load .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Erreur de chargement du fichier .env : %v", err)
-	}
-
-	// Obtenir l'URL du serveur NATS depuis les variables d'environnement
-	natsURL := os.Getenv("NATS_SERVER_URL")
-	if natsURL == "" {
-		log.Fatal("NATS_SERVER_URL n'est pas défini dans les variables d'environnement")
-	}
-
-	// Créer une nouvelle instance de NatsService
-	natsService, err := NewNatsService(natsURL)
-	if err != nil {
-		// Log error and exit
-		log.Fatalf("Erreur lors de la création de NatsService : %v", err)
-	} else {
-		// Log success message
-		fmt.Println("NatsService créé avec succès")
-
-		// S'abonner à un sujet for testing
-		natsService.Subscribe("crypto.news", func(message string) {
-			fmt.Println("Message reçu : ", message)
-		})
-	}
+	RunScraper()
 }
--- src/processor.go
+++ src/processor.go
@@ -1,3 +1,46 @@
 package main
 
-// Gestion de la logique de traitement des données scrappées
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+/**
+ * RunScraper initialise les services NATS et Colly, et lance le scraping en continu.
+ * Cette fonction est appelée depuis le point d'entrée de l'application.
+ * return {void}
+ */
+func RunScraper() {
+	// Charger le fichier .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Erreur de chargement du fichier .env : %v", err)
+	}
+
+	// Obtenir l'URL du serveur NATS depuis les variables d'environnement
+	natsURL := os.Getenv("NATS_SERVER_URL")
+	if natsURL == "" {
+		log.Fatal("NATS_SERVER_URL n'est pas défini dans les variables d'environnement")
+	}
+
+	// Créer une nouvelle instance de NatsService
+	natsService, err := NewNatsService(natsURL)
+	if err != nil {
+		log.Fatalf("Erreur lors de la création de NatsService : %v", err)
+	}
+
+	// Créer une nouvelle instance de CollyService pour le scraping
+	collyService := NewCollyService()
+
+	// Boucle infinie pour effectuer le scraping en continu
+	for {
+		// Démarrer le scraping
+		collyService.ScrapeNews("https://example.com/news", natsService)
+
+		// Attendre 1 minute avant le prochain cycle de scraping
+		time.Sleep(1 * time.Minute)
+	}
+}
