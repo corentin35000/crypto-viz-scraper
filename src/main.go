1dbc1e737cf292bc5eb36ab819d2c5c0585ce83b
Corentin Recanzone <corentindevpro@hotmail.com>
2024-10-28 16:46:53 +0100

    feat: add in main for load env and connect to server nats

--- src/main.go
+++ src/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	_ "github.com/gocolly/colly/v2"
+	"github.com/joho/godotenv"
 	_ "github.com/nats-io/nats.go"
 )
 
 // Point d'entrée de l'application
 func main() {
-	fmt.Println("Hello world !")
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Erreur de chargement du fichier .env : %v", err)
+	}
+
+	// Get the value of the NATS_SERVER_URL environment variable
+	natsURL := os.Getenv("NATS_SERVER_URL")
+	NewNatsService(natsURL)
+
+	// Print a message
+	fmt.Printf("Hello, world!\n")
 }
